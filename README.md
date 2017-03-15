226c4193467e2ed6ffc7faa41e8f9e073b9f4490
Nikhil Bhaskar <nikhil38@gmail.com>
2017-03-14 20:16:51 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# Self Driving Car: Find all collision courses given a N X M grid/array representing a road
+
+## Problem
+
+Among the many things a self-driving car must consider when driving, the car has to ensure it <strong>doesn’t collide</strong> with any other cars.
+
+We can represent a road as an N X M grid. How can we tell whether 2 cars are on a collision course with each other?
+
+Take a look at these short animations and see if you notice something.
+
+## Collision
+![demo](images/collision.gif)
+
+## Safe
+![demo](images/safe.gif)
+
+The key? Cars that are on a collision course are on the same diagonal!
+
+## Diagonal
+## Collision
+![demo](images/diagonal.png)
+
+## Usage
+<ol>
+  <li>Clone the repo</li>
+  <li>Open index.html in the browser</li>
+  <li>Open the console (right-click -> Inspect/Inspect Element) and see the output</li>
+</ol>
+
+## Input
+### A 2D array, with 1s and 0s
+<ul>
+  <li>1 signifies there is a car at the coordinate</li>
+  <li>0 signifies the coordinate is empty</li>
+</ul>
+
+'''javascript
+  [0, 1, 1, 1, 0, 0, 1, 1, 0, 0]
+  [1, 1, 1, 1, 1, 0, 0, 1, 1, 1]
+  [0, 1, 0, 0, 0, 0, 1, 0, 1, 1]
+  [0, 1, 0, 1, 1, 0, 0, 1, 0, 0]
+  [1, 1, 0, 0, 0, 1, 1, 0, 1, 1]
+  [1, 0, 0, 1, 0, 0, 1, 0, 0, 1]
+  [1, 1, 0, 1, 1, 1, 0, 1, 0, 1]
+  [1, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+  [1, 0, 1, 1, 1, 1, 1, 0, 0, 0]
+  [1, 0, 0, 0, 1, 1, 1, 1, 0, 1]
+'''
+
+## Output
+### An Object, with key as stringed coordinates, and value as an an array of coordinates which are collision courses
+#### Example: The collision courses of (0,0) are coordinates (1,1), (3,3) & (9,9) because (1,1), (3,3) & (9,9) have cars in them
+'''javascript
+
+  {
+    0,0: [[1, 1], [3, 3], [9, 9]],
+    0,1: [[1, 2], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [1, 0]],
+    0,2: [[1, 3], [4, 6], [1, 1]],
+    0,3: [[1, 4], [6, 9], [1, 2], [2, 1]],
+    .....
+  }
+'''
+
+## Assumptions
+### All cars in the same road are moving at the exact same speed. We make this assumption for 2 reasons
+<ol>
+  <li>They are on the same road space (same speed limits)  </li>
+  <li>They are autonomous (can control the exact speed they go)</li>
+</ol>
+
+## In data structure terms
+
+Given a 2D array, find all diagonals of all coordinates.
+
+More screenshots: [without text](Screenshots/empty.png) and [with
+text](Screenshots/text.png).
+
+## To add
+<ol>
+  <li>Unit tests</li>
+  <li>More animations</li>
+  <li>More complexity</li>
+</ol>
+
+### Author
+
+Nikhil Bhaskar ([@fphilipe](http://twitter.com/fphilipe))
---
+++ index.js
@@ -0,0 +1,137 @@
+
+/*
+
+Creates an array of any dimensions:
+
+Examples:
+createArray() => []
+createArray(2) => new Array(2)
+createArray(3, 2) => [new Array(2),
+                      new Array(2),
+                      new Array(2)]
+*/
+function createArray(size) {
+  const rows = size;
+  const array = new Array(rows);
+
+  if (arguments.length > 1) {
+    const remainingArgs = Array.prototype.slice.call(arguments, 1);
+    for (let i = 0; i < array.length; i++) {
+      array[i] = createArray.apply(this, remainingArgs);
+    }
+  }
+  return array;
+}
+
+/*
+  Checks whether the argument is an array
+*/
+function isArray(cand) {
+  return cand && cand.constructor === Array;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution
+ */
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/*
+  initializes an array of any dimensions, calling the passed in function 'fn' for every element
+*/
+function initializeArray(arr, fn = v => {
+  return v;
+}) {
+  for (let i = 0; i < arr.length; i++) {
+    const el = arr[i];
+    if (isArray(el)) {
+      initializeArray(el, fn);
+    }else {
+      arr[i] = fn(i);
+    }
+  }
+
+  return arr;
+}
+
+let trafficMatrix = createArray(10, 10);
+trafficMatrix = initializeArray(
+  trafficMatrix,
+  idx => {
+    return getRandomInt(0, 1);
+  }
+);
+
+console.log('The traffic matrix is....')
+console.log(trafficMatrix)
+console.log('\n');
+
+console.log('The collision courses of the above traffic matrix is...');
+console.log(findCollisionCourses(trafficMatrix));
+
+/*
+As an example, look at the matrix
+
+ [0, 0, 0, 0, 0
+  0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0]
+
+Take coordinate (0, 0);
+Coordinates in its collision course are (1,1), (2,2), (3,3), (4,4) & (5,5)
+Why? Well, since all cars are self driven, they must all be going at the same speed in an intersection
+*/
+function findCollisionCourses(matrix) {
+  const collisionCourses = {};
+  matrix.forEach((row, rowIdx) => {
+    row.forEach((el, colIdx) => {
+      collisionCourses[
+        [rowIdx, colIdx]
+      ] = findSquareMatricesWithCorner(
+        matrix,
+        [rowIdx, colIdx]
+      );
+    })
+  })
+
+  return collisionCourses;
+}
+
+function isUndefined(cand) {
+  return typeof cand === 'undefined';
+}
+
+//cache ?
+function findSquareMatricesWithCorner(matrix, [rowIdx, colIdx]) {
+  const out = [];
+  const [originalRowIdx, originalColIdx] = [rowIdx, colIdx];
+  const DIRECTIONS = [
+    [1, 1], //bottom right
+    [1, -1], //bottom left
+    [-1, -1], //top left
+    [-1, 1], //top right
+  ];
+
+  DIRECTIONS.forEach(direction => {
+    while(rowIdx < matrix.length && colIdx < matrix[rowIdx].length) {
+      rowIdx += direction[0];
+      colIdx += direction[1];
+
+      const collisionCand = matrix[rowIdx] && matrix[rowIdx][colIdx];
+
+      if (isUndefined(collisionCand)) {
+        break;
+      }
+
+      if (collisionCand === 1) {
+        out.push([rowIdx,colIdx]);
+      }
+    }
+    [rowIdx, colIdx] = [originalRowIdx, originalColIdx];
+  });
+
+  return out;
+}
